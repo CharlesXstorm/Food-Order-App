064e99e1c5a844bf7f05ab9feabf4de78835f232
Charles Ukadike <charlesukadike44@gmail.com>
2023-02-24 15:43:26 +0100

    updated scroll animation

--- src/components/UI/Animarrow.js
+++ src/components/UI/Animarrow.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import style from './Animarrow.module.css';
 
+/*
+<div className={style.arrow1}>
+        <div className={style.line1}></div>
+        <div className={style.line2}></div>
+      </div>
+      <div className={style.arrow2}>
+        <div className={style.line1}></div>
+        <div className={style.line2}></div>
+      </div>
+      <div className={style.arrow3}>
+        <div className={style.line1}></div>
+        <div className={style.line2}></div>
+      </div>
+*/
+
 function Animarrow() {
   return (
     <div className={style.animarrow}>
-        <div className={style.arrow1}>
-            <div className={style.line1}></div>
-            <div className={style.line2}></div>
-        </div>
-        <div className={style.arrow2}>
-            <div className={style.line1}></div>
-            <div className={style.line2}></div>
-        </div>
-        <div className={style.arrow3}>
-            <div className={style.line1}></div>
-            <div className={style.line2}></div>
-        </div>
+      <span className={`material-symbols-outlined ${style.chev}`}>
+        keyboard_double_arrow_down
+      </span>
 
     </div>
   )
